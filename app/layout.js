8b96bfb64318fcd9daf9f04084227dbb56c1ecf2
RossWebFS <161938898+RossWebFS@users.noreply.github.com>
2024-08-20 21:36:21 +0300

    Added global styles and font

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,21 @@
 import Navigation from "@/app/_components/Navigation";
 import Logo from "@/app/_components/Logo";
 
+import "@/app/_styles/global.css";
+
+import { Josefin_Sans } from "next/font/google";
+
+const josefinFont = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-      <body>
+      <body
+        className={`${josefinFont.className} bg-primary-950 text-primary-100 min-h-screen`}
+      >
         <header>
           <Logo />
           <Navigation />
