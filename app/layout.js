399683e110a6e2daa19905edc8dc053a38c76af0
RossWebFS <shimon.vip2006@gmail.com>
2024-08-20 21:32:13 +0300

    Added default font

--- app/layout.js
+++ app/layout.js
@@ -3,10 +3,19 @@ import Logo from "@/app/_components/Logo";
 
 import "@/app/_styles/global.css";
 
+import { Josefin_Sans } from "next/font/google";
+
+const josefinFont = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
+      <body
+        className={`${josefinFont.className} bg-primary-950 text-primary-100 min-h-screen`}
+      >
         <header>
           <Logo />
           <Navigation />
