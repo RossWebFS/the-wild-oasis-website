19f99e2ec22fbed403dca10e62fc903d772f8f80
RossWebFS <shimon.vip2006@gmail.com>
2024-08-20 08:32:56 +0300

    Built root layout

--- app/page.js
+++ app/page.js
@@ -1,12 +1,5 @@
-import Navigation from "@/app/_components/Navigation";
-
 const Page = () => {
-  return (
-    <>
-      <Navigation />
-      <h1>The Wild Oasis Website</h1>
-    </>
-  );
+  return <h1>The Wild Oasis Website</h1>;
 };
 
 export default Page;
