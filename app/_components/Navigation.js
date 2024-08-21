43a91d4cfccacbce3f347965b6312714315e9cc6
RossWebFS <shimon.vip2006@gmail.com>
2024-08-21 09:45:43 +0300

    Styled app layout, added sidebar

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -2,19 +2,27 @@ import Link from "next/link";
 
 const Navigation = () => {
   return (
-    <nav>
-      <ul>
+    <nav className="z-10 text-xl">
+      <ul className="flex items-center gap-16">
         <li>
-          <Link href="/cabins">Cabins</Link>
+          <NavItem href="/cabins">Cabins</NavItem>
         </li>
         <li>
-          <Link href="/account">Accout</Link>
+          <NavItem href="/about">About</NavItem>
         </li>
         <li>
-          <Link href="/about">About</Link>
+          <NavItem href="/account">Guest area</NavItem>
         </li>
       </ul>
     </nav>
+  );
+};
+
+const NavItem = ({ href, children }) => {
+  return (
+    <Link href={href} className="hover:text-accent-400 transition-colors">
+      {children}
+    </Link>
   );
 };
 
--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,8 @@
-import Navigation from "@/app/_components/Navigation";
-import Logo from "@/app/_components/Logo";
+import { Josefin_Sans } from "next/font/google";
+
+import Header from "@/app/_components/Header";
 
 import "@/app/_styles/global.css";
-
-import { Josefin_Sans } from "next/font/google";
 
 const josefinFont = Josefin_Sans({
   subsets: ["latin"],
@@ -23,14 +22,12 @@ const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body
-        className={`${josefinFont.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefinFont.className} bg-primary-950 text-primary-100 relative flex min-h-screen flex-col antialiased`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+        <Header />
+        <div className="grid flex-1 px-8 py-12">
+          <main className="mx-auto w-full max-w-7xl">{children}</main>
+        </div>
       </body>
     </html>
   );
