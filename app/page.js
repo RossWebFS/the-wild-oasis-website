e042e963f4745f923cb36c1382a7e14797f79b21
RossWebFS <shimon.vip2006@gmail.com>
2024-08-21 22:37:53 +0300

    Created home page

--- app/page.js
+++ app/page.js
@@ -1,5 +1,32 @@
+import Image from "next/image";
+
+import bg from "@/public/bg.png";
+
 const Page = () => {
-  return <h1>The Wild Oasis Website</h1>;
+  return (
+    <main className="mt-24">
+      <Image
+        src={bg}
+        className="object-cover object-top"
+        placeholder="blur"
+        fill
+        quality={80}
+        alt="Mountains and forests with two cabins"
+      />
+
+      <div className="relative z-10 text-center">
+        <h1 className="text-primary-50 mb-10 text-8xl font-normal tracking-tight">
+          Welcome to paradise.
+        </h1>
+        <a
+          href="/cabins"
+          className="bg-accent-500 text-primary-800 hover:bg-accent-600 px-8 py-6 text-lg font-semibold transition-all"
+        >
+          Explore luxury cabins
+        </a>
+      </div>
+    </main>
+  );
 };
 
 export default Page;
