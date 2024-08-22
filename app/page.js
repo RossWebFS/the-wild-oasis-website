d30daad96fca96981d62a4f48f6c655722543b30
RossWebFS <shimon.vip2006@gmail.com>
2024-08-22 19:21:59 +0300

    Built not found page

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import bg from "@/public/bg.png";
 
@@ -18,12 +19,12 @@ const Page = () => {
         <h1 className="text-primary-50 mb-10 text-8xl font-normal tracking-tight">
           Welcome to paradise.
         </h1>
-        <a
+        <Link
           href="/cabins"
           className="bg-accent-500 text-primary-800 hover:bg-accent-600 px-8 py-6 text-lg font-semibold transition-all"
         >
           Explore luxury cabins
-        </a>
+        </Link>
       </div>
     </main>
   );
