79a85a3645532db524bb9782ed1a5c4787339e92
RossWebFS <shimon.vip2006@gmail.com>
2024-08-22 12:22:09 +0300

    Fixed cabin card component, added image configuration

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -22,13 +22,13 @@ const Page = async () => {
         Welcome to paradise.
       </p>
 
-      {/* {cabins.length > 0 && (
+      {cabins.length > 0 && (
         <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:gap-12 xl:gap-14">
           {cabins.map(cabin => (
             <CabinCard cabin={cabin} key={cabin.id} />
           ))}
         </div>
-      )} */}
+      )}
     </div>
   );
 };
