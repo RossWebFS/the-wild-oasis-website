f83b33878eae2ef1fc23519fd72a6738f108e2bb
RossWebFS <shimon.vip2006@gmail.com>
2024-08-22 11:16:53 +0300

    Added fetching cabins

---
+++ app/_lib/data-services.js
@@ -0,0 +1,48 @@
+import { eachDayOfInterval } from "date-fns";
+
+import { supabase } from "@/app/_lib/supabase";
+
+// GET
+export const getCabin = async id => {
+  const { data, error } = await supabase
+    .from("cabins")
+    .select("*")
+    .eq("id", id)
+    .single();
+
+  if (error) {
+    console.error(error);
+  }
+
+  return data;
+};
+
+export const getCabinPrice = async id => {
+  const { data, error } = await supabase
+    .from("cabins")
+    .select("regularPrice, discount")
+    .eq("id", id)
+    .single();
+
+  if (error) {
+    console.error(error);
+  }
+
+  return data;
+};
+
+export const getCabins = async () => {
+  const { data, error } = await supabase
+    .from("cabins")
+    .select("id, name, maxCapacity, regularPrice, discount, image")
+    .order("name");
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could not be loaded");
+  }
+
+  console.log(data);
+
+  return data;
+};
--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,23 +1,12 @@
 import CabinCard from "@/app/_components/CabinCard";
+import { getCabins } from "@/app/_lib/data-services";
 
 export const metadata = {
   title: "Cabins",
 };
 
-const Page = () => {
-  // CHANGE
-  const cabins = [
-    {
-      id: "gmegklngld",
-      name: "001",
-      maxCapacity: 2,
-      regularPrice: 250,
-      discount: 0,
-      image: "https://jhgrtuwkfqnleofxjvdb.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg",
-      description:
-        "Discover the ultimate luxury getaway for couples in the cozy wooden cabin 001. Nestled in a picturesque forest, this stunning cabin offers a secluded and intimate retreat. Inside, enjoy modern high-quality wood interiors, a comfortable seating area, a fireplace and a fully-equipped kitchen. The plush king-size bed, dressed in fine linens guarantees a peaceful nights sleep. Relax in the spa-like shower and unwind on the private deck with hot tub.",
-    },
-  ];
+const Page = async () => {
+  const cabins = await getCabins();
 
   return (
     <div>
@@ -33,13 +22,13 @@ const Page = () => {
         Welcome to paradise.
       </p>
 
-      {cabins.length > 0 && (
+      {/* {cabins.length > 0 && (
         <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:gap-12 xl:gap-14">
           {cabins.map(cabin => (
             <CabinCard cabin={cabin} key={cabin.id} />
           ))}
         </div>
-      )}
+      )} */}
     </div>
   );
 };
