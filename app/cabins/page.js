1701342e22d1cd071d02d24f4733a643549dc483
RossWebFS <shimon.vip2006@gmail.com>
2024-08-22 08:58:30 +0300

    Built cabins page ui

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,9 +1,47 @@
+import CabinCard from "@/app/_components/CabinCard";
+
 export const metadata = {
   title: "Cabins",
 };
 
 const Page = () => {
-  return <div>Cabins</div>;
+  // CHANGE
+  const cabins = [
+    {
+      id: "gmegklngld",
+      name: "001",
+      maxCapacity: 2,
+      regularPrice: 250,
+      discount: 0,
+      image: "https://jhgrtuwkfqnleofxjvdb.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg",
+      description:
+        "Discover the ultimate luxury getaway for couples in the cozy wooden cabin 001. Nestled in a picturesque forest, this stunning cabin offers a secluded and intimate retreat. Inside, enjoy modern high-quality wood interiors, a comfortable seating area, a fireplace and a fully-equipped kitchen. The plush king-size bed, dressed in fine linens guarantees a peaceful nights sleep. Relax in the spa-like shower and unwind on the private deck with hot tub.",
+    },
+  ];
+
+  return (
+    <div>
+      <h1 className="text-accent-400 mb-5 text-4xl font-medium">
+        Our Luxury Cabins
+      </h1>
+      <p className="text-primary-200 mb-10 text-lg">
+        Cozy yet luxurious cabins, located right in the heart of the Italian
+        Dolomites. Imagine waking up to beautiful mountain views, spending your
+        days exploring the dark forests around, or just relaxing in your private
+        hot tub under the stars. Enjoy nature&apos;s beauty in your own little
+        home away from home. The perfect spot for a peaceful, calm vacation.
+        Welcome to paradise.
+      </p>
+
+      {cabins.length > 0 && (
+        <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:gap-12 xl:gap-14">
+          {cabins.map(cabin => (
+            <CabinCard cabin={cabin} key={cabin.id} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Page;
