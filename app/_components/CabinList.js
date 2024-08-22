2611055e79a6c5d3a14d0998511ad44292e08e32
RossWebFS <161938898+RossWebFS@users.noreply.github.com>
2024-08-22 23:57:28 +0300

    Optimized cabin list and cabin page

--- app/_components/CabinList.js
+++ app/_components/CabinList.js
@@ -1,8 +1,12 @@
+import { unstable_noStore as noStore } from "next/cache";
+
 import CabinCard from "@/app/_components/CabinCard";
 
 import { getCabins } from "@/app/_lib/data-services";
 
 const CabinList = async () => {
+  noStore();
+
   const cabins = await getCabins();
 
   if (!cabins.length) return null;
--- app/cabins/page.js
+++ app/cabins/page.js
@@ -10,10 +10,10 @@ export const metadata = {
 const Page = () => {
   return (
     <div>
-      <h1 className="text-accent-400 mb-5 text-4xl font-medium">
+      <h1 className="mb-5 text-4xl font-medium text-accent-400">
         Our Luxury Cabins
       </h1>
-      <p className="text-primary-200 mb-10 text-lg">
+      <p className="mb-10 text-lg text-primary-200">
         Cozy yet luxurious cabins, located right in the heart of the Italian
         Dolomites. Imagine waking up to beautiful mountain views, spending your
         days exploring the dark forests around, or just relaxing in your private
