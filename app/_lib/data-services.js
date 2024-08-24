b7ce334e4959f72009cda6534e145ccb53d5538d
RossWebFS <shimon.vip2006@gmail.com>
2024-08-24 22:01:22 +0300

    built profile page

--- app/_lib/data-services.js
+++ app/_lib/data-services.js
@@ -45,3 +45,15 @@ export const getCabins = async () => {
 
   return data;
 };
+
+export const getCountries = async () => {
+  try {
+    const res = await fetch(
+      "https://restcountries.com/v2/all?fields=name,flag",
+    );
+    const countries = await res.json();
+    return countries;
+  } catch {
+    throw new Error("Could not fetch countries");
+  }
+};
