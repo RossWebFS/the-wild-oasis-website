3aedd529f469fade27372c97b5affe091592a68f
RossWebFS <shimon.vip2006@gmail.com>
2024-08-25 21:55:08 +0300

    Added guest to database

--- app/_lib/data-services.js
+++ app/_lib/data-services.js
@@ -99,3 +99,26 @@ export const getSettings = async () => {
 
   return data;
 };
+
+export const getGuest = async email => {
+  const { data, error } = await supabase
+    .from("guests")
+    .select("*")
+    .eq("email", email)
+    .single();
+
+  // No error here! We handle the possibility of no guest in the sign in callback
+  return data;
+};
+
+// CREATE
+export const createGuest = async newGuest => {
+  const { data, error } = await supabase.from("guests").insert([newGuest]);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Guest could not be created");
+  }
+
+  return data;
+};
